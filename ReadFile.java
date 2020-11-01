84c901dbff18c2bf25f24e37c1ab735bcd966ba7
melj456 <64508945+melj456@users.noreply.github.com>
2020-11-01 14:33:07 -0500

    Add files via upload

---
+++ ReadFile.java
@@ -0,0 +1,99 @@
+package readfilePractice;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ReadFile {
+
+	public static void main(String[] args) throws IOException {
+		boolean shutdown = false;
+		
+		
+			ServerSocket ss = new ServerSocket(8080, 1, InetAddress.getByName("127.0.0.1")); 
+			Socket socket = ss.accept();
+			System.out.println("Connection from: " + socket);
+			
+			OutputStream outputStream = socket.getOutputStream();
+	        ObjectOutputStream dataOutputStream = new ObjectOutputStream(outputStream);
+			
+			InputStream inputStream = socket.getInputStream();
+			DataInputStream dataInputStream = new DataInputStream(inputStream);			
+			String message = dataInputStream.readUTF();
+//	        System.out.println("The message from client was: " + message);
+	        	        
+	        writeToClient(dataOutputStream, message);
+	        
+	        System.out.println("Closing sockets.");	
+	        outputStream.close();
+	        inputStream.close();
+	        ss.close();
+	        socket.close();
+	         
+	
+}
+	
+		static public void writeToClient(ObjectOutputStream outputStream, String message) throws IOException {
+			read r = new read("chr1.fa");
+			outputStream.writeObject((r.Search(message)));
+			
+			System.out.println("Writing to client...");
+	        outputStream.flush();	         
+		}
+		
+}
+
+
+class read {
+	String file;
+	read(String file){
+		this.file = file;
+	}
+	
+	public List<Integer> Search(String gene) {
+		List <Integer> list = Collections.synchronizedList(new ArrayList<Integer>());
+		
+		try {
+			
+			BufferedReader reader =new BufferedReader(new FileReader(file));
+			StringBuilder sb = new StringBuilder();
+			sb.append(gene);
+			String str;
+			
+			while((str = reader.readLine()) != null) {
+				sb.append(str.toUpperCase());				
+			}
+			
+			sb.delete(0, gene.length());
+			
+			int index = 0;
+			while(index >= 0) {
+				index = sb.indexOf(gene, index + 1);
+				if(index >= 0) {
+					list.add(index);
+				}
+			}
+					
+//			System.out.println("Index list: " + list);
+//			System.out.println("Size: " + list.size());
+			
+			reader.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+}
+
---
+++ guiClientTest.java
@@ -0,0 +1,137 @@
+package guiClientTest;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class guiClientTest {
+	private static void constructGUI() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		myFrame frame = new myFrame();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				constructGUI();
+			}
+		});
+ }
+
+static class myFrame extends JFrame implements ActionListener {
+	static public JLabel label1;
+	static public JList jList;
+	public JButton button1;
+	public JTextField textField;
+	
+	myFrame() {
+		super();
+		init();
+	}
+	
+	private void init() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame frame = new JFrame("Gene Client");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JComponent panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		
+		jList = new JList();
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.getViewport().add(jList, BorderLayout.CENTER);
+		
+		button1 = new JButton("Send");
+		textField = new JTextField("", 20);
+		label1 = new JLabel("Total Items: ");
+		panel.add(new JLabel("Enter Gene Sequence Here: "));
+		panel.add(textField);
+		panel.add(button1);
+		button1.addActionListener(this);
+		
+		this.add(panel, BorderLayout.NORTH);
+		this.add(scrollPane, BorderLayout.CENTER);
+		this.add(label1, BorderLayout.SOUTH);
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		String userInput;
+		JButton source = (JButton) e.getSource();
+		String s = source.getText();
+		if(s == "Send") {
+			userInput = textField.getText();
+			try {
+				connect(userInput);
+			} catch (ClassNotFoundException e1) {			
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+	}
+	
+	static private void connect(String userInput) throws IOException, ClassNotFoundException {
+		Socket serverConnection = new Socket(InetAddress.getByName("127.0.0.1"), 8080);
+		System.out.println("Connected");
+		 
+		OutputStream outputStream = serverConnection.getOutputStream();
+		DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+		 
+		InputStream inputStream = serverConnection.getInputStream();
+	    ObjectInputStream dataInputStream = new ObjectInputStream(inputStream);	     
+	     
+	    writeToServer(dataOutputStream, userInput);
+	    readFromServer(dataInputStream);
+	     
+	    System.out.println("Closing sockets.");
+	    outputStream.close();
+	    inputStream.close();
+	    serverConnection.close();
+	}
+	
+	static public void writeToServer(DataOutputStream outputStream, String userInput) throws IOException {
+	    String geneString = userInput.replaceAll("\\s", ""); 
+	    outputStream.writeUTF(geneString.toUpperCase());
+	    outputStream.flush();	   
+		
+	}
+	
+	static public void readFromServer(ObjectInputStream inputStream) throws IOException, ClassNotFoundException {
+		List <Integer> intList = Collections.synchronizedList(new ArrayList<Integer>());
+		intList = (List<Integer>) inputStream.readObject();
+		
+		DefaultListModel listModel = new DefaultListModel();
+		for(int i = 0; i < intList.size(); i++) {
+			listModel.addElement(intList.get(i));
+		}
+		
+		jList.setModel(listModel);
+		label1.setText("Total Results: " + String.valueOf(intList.size()));
+//		System.out.print("Message from server: ");
+//		System.out.println(intList);
+//		System.out.println("Size: " + intList.size());
+			
+	}	
+  }
+}
+
+
+
+
+
